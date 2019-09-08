cf3fffb8b82c7c4cfd18a35d8a11b4972927e150
eveloso-src <elemudela@gmail.com>
2019-09-08 20:27:24 -0300

    Base commit

---
+++ src/main/java/com/muebles/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.muebles.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.muebles.model.Role;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+	Role findByRole(String role);
+
+}
